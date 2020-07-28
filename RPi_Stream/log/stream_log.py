e972e1c1ea156081695cc5e15cce7f88bf8c2f96
aliaagouda <goudaliaa@gmail.com>
2020-07-28 09:40:39 -0400

    added streaming and logging

---
+++ RPi_Stream/log/stream_log.py
@@ -0,0 +1,228 @@
+# usage: python stream_acc.py [mac1] [mac2] ... [mac(n)]
+from __future__ import print_function
+from ctypes import c_void_p, cast, POINTER
+from mbientlab.metawear import MetaWear, libmetawear, parse_value, create_voidp
+from mbientlab.metawear import cbindings as metacbindings
+from mbientlab.metawear.cbindings import *
+from mbientlab.warble import *
+from time import sleep
+from threading import Event
+from sys import argv
+import numpy as np
+import os
+import csv
+import platform
+import six
+
+if sys.version_info[0] == 2:
+    range = xrange
+
+class State:
+    def __init__(self, device):
+        self.device = device
+        self.samples = 0
+        self.processor = None
+        
+        self.stream_gyro = []
+        self.log_acc_gyro = []
+        self.log_euler = []
+        self.log_baro = []
+        self.log_signals = []
+
+        self.callback = metacbindings.FnVoid_VoidP_DataP(self.stream_data_handler)
+
+    def stream_data_handler(self, ctx, data):
+        values = parse_value(data)
+        print("time: %s" % (data.contents.epoch), values)
+        self.stream_gyro.append([data.contents.epoch, 0, values.x, values.y, values.z])
+        self.samples+= 1
+        
+    def log_acc_gyro_handler(self, ctx, p):
+        values = parse_value(p, n_elem = 2)
+        self.log_acc_gyro.append([p.contents.epoch, 0, values[0].x, values[0].y, values[0].z, values[1].x, values[1].y, values[1].z]) 
+
+    def log_euler_handler(self, ctx, p):
+        values = parse_value(p)
+        self.log_euler.append([p.contents.epoch, 0, values.heading, values.pitch, values.roll, values.yaw]) 
+        
+    def log_baro_handler(self, ctx, p):
+        values = parse_value(p)
+        self.log_baro.append([p.contents.epoch, 0, values]) 
+    
+    def setup(self):
+        libmetawear.mbl_mw_settings_set_connection_parameters(self.device.board, 7.5, 7.5, 0, 6000)
+        sleep(1.5)
+    
+        e = Event()
+    
+        def processor_created(context, pointer):
+            self.processor = pointer
+            e.set()
+        fn_wrapper = metacbindings.FnVoid_VoidP_VoidP(processor_created)
+#---------------------------------------------------------------------------------------------------------------------
+        # set accelerometer to 100Hz sampling rate and range to +/- 16 g's
+        libmetawear.mbl_mw_acc_set_odr(self.device.board, 100.0) 
+        libmetawear.mbl_mw_acc_set_range(self.device.board, 16.0)
+        libmetawear.mbl_mw_acc_write_acceleration_config(self.device.board)
+        
+        # set gyro to 100Hz sampling rate and +/- 1000 deg/sec. 
+        libmetawear.mbl_mw_gyro_bmi160_set_odr(self.device.board, GyroBmi160Odr._100Hz) 
+        libmetawear.mbl_mw_gyro_bmi160_set_range(self.device.board, GyroBmi160Range._2000dps)
+        libmetawear.mbl_mw_gyro_bmi160_write_config(self.device.board)
+        
+        # set sensor_fusion settings 
+        libmetawear.mbl_mw_sensor_fusion_set_mode(self.device.board, SensorFusionMode.NDOF);
+        libmetawear.mbl_mw_sensor_fusion_write_config(self.device.board)
+    
+        #~ # set baro to 10ms (= 100Hz sampling rate)
+        libmetawear.mbl_mw_baro_bosch_set_standby_time(self.device.board, 10.0)
+        libmetawear.mbl_mw_baro_bmp280_set_standby_time(self.device.board, BaroBmp280StandbyTime._62_5ms)
+        libmetawear.mbl_mw_baro_bosch_write_config(self.device.board)
+    
+        # get pointers referencing the acc and gyro data signals
+        acc = libmetawear.mbl_mw_acc_get_acceleration_data_signal(self.device.board)
+        gyro = libmetawear.mbl_mw_gyro_bmi160_get_rotation_data_signal(self.device.board)
+        euler = libmetawear.mbl_mw_sensor_fusion_get_data_signal(self.device.board, SensorFusionData.EULER_ANGLE)
+        baro = libmetawear.mbl_mw_baro_bosch_get_pressure_data_signal(self.device.board)
+            
+        signals = (c_void_p * 1)()
+        signals[0] = gyro
+     
+        # chain two processors together (fuser and accounter) to get timestamped acc+gyro data
+        # create a fuser "data processor" which packages the acc and gyro signals into same packets before sending
+        fuser = create_voidp(lambda fn: libmetawear.mbl_mw_dataprocessor_fuser_create(acc, signals, 1, None, fn), resource = "fuser", event = e)
+        
+        # accounter processor adds correct epoch data to BLE packets, necessary for timestamping stream-mode data
+        accounter = create_voidp(lambda fn: libmetawear.mbl_mw_dataprocessor_accounter_create(gyro, None, fn), resource = "accounter", event = e)
+    
+        libmetawear.mbl_mw_datasignal_subscribe(accounter, None, self.callback)
+        
+        logger_acc_gyro = create_voidp(lambda fn: libmetawear.mbl_mw_datasignal_log(fuser, None, fn), resource = "acc_gyro_logger")
+        logger_euler = create_voidp(lambda fn: libmetawear.mbl_mw_datasignal_log(euler, None, fn), resource = "euler_logger")
+        logger_baro = create_voidp(lambda fn: libmetawear.mbl_mw_datasignal_log(baro, None, fn), resource = "baro_logger")
+        self.log_signals = [logger_acc_gyro, logger_euler, logger_baro]
+
+    def start(self):
+        libmetawear.mbl_mw_logging_start(self.device.board, 0)
+        
+        libmetawear.mbl_mw_acc_enable_acceleration_sampling(self.device.board)
+        libmetawear.mbl_mw_gyro_bmi160_enable_rotation_sampling(self.device.board)
+        libmetawear.mbl_mw_sensor_fusion_enable_data(self.device.board, SensorFusionData.EULER_ANGLE)
+        
+        libmetawear.mbl_mw_acc_start(self.device.board)
+        libmetawear.mbl_mw_gyro_bmi160_start(self.device.board)
+        libmetawear.mbl_mw_sensor_fusion_start(self.device.board)
+        libmetawear.mbl_mw_baro_bosch_start(self.device.board)
+        
+        print("Logging data for 15s")
+        
+    def stop(self):
+        libmetawear.mbl_mw_acc_stop(self.device.board)
+        libmetawear.mbl_mw_acc_disable_acceleration_sampling(self.device.board)
+
+        libmetawear.mbl_mw_gyro_bmi160_stop(self.device.board)
+        libmetawear.mbl_mw_gyro_bmi160_disable_rotation_sampling(self.device.board)
+        
+        libmetawear.mbl_mw_sensor_fusion_stop(self.device.board);
+        signal = libmetawear.mbl_mw_sensor_fusion_get_data_signal(self.device.board, SensorFusionData.EULER_ANGLE);
+        libmetawear.mbl_mw_datasignal_unsubscribe(signal)
+     
+        libmetawear.mbl_mw_baro_bosch_stop(self.device.board)
+        
+        libmetawear.mbl_mw_logging_stop(self.device.board)
+
+#---------------------------------------------------------------------------------------------------------------------
+        print("Downloading data")
+
+        e = Event()
+        def progress_update_handler(context, entries_left, total_entries):
+            if (entries_left == 0):
+                e.set()
+        
+        fn_wrapper = FnVoid_VoidP_UInt_UInt(progress_update_handler)
+        download_handler = LogDownloadHandler(context = None, \
+            received_progress_update = fn_wrapper, \
+            received_unknown_entry = cast(None, FnVoid_VoidP_UByte_Long_UByteP_UByte), \
+            received_unhandled_entry = cast(None, FnVoid_VoidP_DataP))
+        
+        log_callback = [FnVoid_VoidP_DataP(self.log_acc_gyro_handler), FnVoid_VoidP_DataP(self.log_euler_handler), FnVoid_VoidP_DataP(self.log_baro_handler)]
+        
+        for i, signal in enumerate(self.log_signals):
+            libmetawear.mbl_mw_logger_subscribe(signal, None, log_callback[i])
+            libmetawear.mbl_mw_logging_download(self.device.board, 0, byref(download_handler))   
+            
+        e.wait()
+        print("here")       
+        
+states = []
+for i in range(len(sys.argv) - 1):
+    d = MetaWear(sys.argv[i + 1])
+    d.connect()
+    print("Connected to " + d.address)
+    states.append(State(d))
+
+for s in states:
+    print("Configuring %s" % (s.device.address))
+    s.setup()
+    s.start()
+
+sleep(2.0)
+
+for s in states:
+    s.stop()
+    
+#---------------------------------------------------------------------------------------------------------------------
+
+trial_name = input("\nPlease enter trial name: ")
+
+for idx, s in enumerate(states):
+    sets = [s.stream_gyro, s.log_acc_gyro, s.log_euler, s.log_baro]
+    set_name = ['stream_gyro', 'log_acc_gyro', 'log_euler', 'log_baro']
+            
+    for i, array in enumerate(sets): 
+        # time based off epoch, stored in ms.
+        # remove offset and convert elapsed time to seconds
+        array = np.asarray(array, dtype=np.float64)
+        timeStart = array[0,0]
+        array[:,1] = [(x - timeStart) / 1000 for x in array[:,0]]
+
+        filename = '../TestData/' + trial_name + '_' + set_name[i] + '_' + str(idx) + '.csv'
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        # write each sensor's data to a separate .csv file
+        with open(filename, 'w') as f:
+            if i == 0:
+                f.write('epoch, elapseds, xgyro, ygyro, zgyro\n')
+                for row in array:
+                    f.write('%d,%.3f,%.4f,%.4f,%.4f\n' % (row[0], row[1], row[2], row[3], row[4]))
+            elif i == 1:
+                f.write('epoch, elapseds, xacc, yacc, zacc, xgyro, ygyro, zgyro\n') 
+                for row in array:
+                    f.write('%d,%.3f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
+            elif i == 2:
+                f.write('epoch, elapseds, heading, pitch, roll, yaw\n')
+                for row in array:
+                    f.write('%d,%.3f,%.4f,%.4f,%.4f,%.4f\n' % (row[0], row[1], row[2], row[3], row[4], row[5]))
+            else:
+                f.write('epoch, elapseds, pressure\n')
+                for row in array:
+                    f.write('%d,%.3f,%.4f\n' % (row[0], row[1], row[2]))
+
+        print("Data saved to \'" + filename + "\'", len(array))
+            
+print("\nTotal Samples Received")
+for s in states:
+    print("%s -> %d" % (s.device.address, s.samples))
+    
+print("Resetting devices")
+events = []
+for s in states:
+    e = Event()
+    events.append(e)
+
+    s.device.on_disconnect = lambda s: e.set()
+    libmetawear.mbl_mw_debug_reset(s.device.board)
+
+for e in events:
+    e.wait()
+
---
+++ RPi_Stream/stream_modes.py
@@ -0,0 +1,237 @@
+# usage: sudo python3 data_fuser.py [mac1] [mac2] ... [mac(n)]
+from __future__ import print_function
+from ctypes import c_void_p, cast, POINTER
+from mbientlab.metawear import MetaWear, libmetawear, parse_value, create_voidp
+from mbientlab.metawear import cbindings as metacbindings
+from mbientlab.warble import *
+from time import sleep
+from threading import Event
+from sys import argv
+import numpy as np
+import os
+
+import platform
+import six
+
+import socket
+
+UDP_IP = '192.168.5.22'
+
+# Available output data rates on the BMI160 gyro
+MBL_MW_GYRO_BMI160_ODR_25Hz= 6
+MBL_MW_GYRO_BMI160_ODR_50Hz= 7
+MBL_MW_GYRO_BMI160_ODR_100Hz= 8
+MBL_MW_GYRO_BMI160_ODR_200Hz= 9
+MBL_MW_GYRO_BMI160_ODR_400Hz= 10
+MBL_MW_GYRO_BMI160_ODR_800Hz= 11
+MBL_MW_GYRO_BMI160_ODR_1600Hz= 12
+MBL_MW_GYRO_BMI160_ODR_3200Hz= 13
+
+# Available degrees per second ranges on the BMI160 gyro
+MBL_MW_GYRO_BMI160_RANGE_2000dps= 0      # +/-2000 degrees per second
+MBL_MW_GYRO_BMI160_RANGE_1000dps= 1      # +/-1000 degrees per second
+MBL_MW_GYRO_BMI160_RANGE_500dps= 2       # +/-500 degrees per second
+MBL_MW_GYRO_BMI160_RANGE_250dps= 3       # +/-250 degrees per second
+MBL_MW_GYRO_BMI160_RANGE_125dps= 4       # +/-125 degrees per second
+
+states = []
+ports = [5005, 5010, 5011, 5025]
+macIDs = sys.argv[1:]
+
+class State:
+    #initialize object for a connected device
+    def __init__(self, device):
+        self.device = device
+        self.callback = metacbindings.FnVoid_VoidP_DataP(self.data_handler)
+        self.processor = None
+        self.sensor_data = []
+        self.samples = 0
+        self.mode = 0
+        
+        #assign unique port from ports list based on device address's index in macIDs list
+        self.UDP_PORT = [ports[i] for i, val in enumerate(macIDs) if val == device.address][0]
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP packet sending
+
+
+    # once subscribe is called, data_handler linked to device data stream
+    # data_handler continuously reads data buffer
+    def data_handler(self, ctx, data):
+        values = parse_value(data, n_elem = 2)
+        
+        self.sensor_data.append([data.contents.epoch, 0, values[0].x, values[0].y, values[0].z, values[1].x, values[1].y, values[1].z]) 
+            
+        # tracks total samples received by pi from sensor
+        self.samples += 1
+
+        MESSAGE = str(values[1].z)
+        
+        self.sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, self.UDP_PORT))
+
+    def setup(self):
+        # set BLE connection params (min connect interval, max interval,
+        # latency, timeout) all in milliseconds
+        libmetawear.mbl_mw_settings_set_connection_parameters(self.device.board, 7.5, 7.5, 0, 6000)
+        # set BLE advertising strength (higher strength = higher power consumption
+        # but theoretically better connectivity)
+        libmetawear.mbl_mw_settings_set_tx_power(self.device.board, 4)
+        sleep(1.5)
+
+        e = Event()
+
+        def processor_created(context, pointer):
+            self.processor = pointer
+            e.set()
+        fn_wrapper = metacbindings.FnVoid_VoidP_VoidP(processor_created)
+
+        # set accelerometer to 100Hz sampling rate and range to +/- 16 g's
+        libmetawear.mbl_mw_acc_set_odr(self.device.board, 100.0) 
+        libmetawear.mbl_mw_acc_set_range(self.device.board, 16.0)
+        libmetawear.mbl_mw_acc_write_acceleration_config(self.device.board)
+        
+        # set gyro to 100Hz sampling rate and +/- 1000 deg/sec. 
+        libmetawear.mbl_mw_gyro_bmi160_set_odr(self.device.board, MBL_MW_GYRO_BMI160_ODR_100Hz) 
+        libmetawear.mbl_mw_gyro_bmi160_set_range(self.device.board, MBL_MW_GYRO_BMI160_RANGE_1000dps)
+        libmetawear.mbl_mw_gyro_bmi160_write_config(self.device.board)
+
+        # get pointers referencing the acc and gyro data signals
+        acc = libmetawear.mbl_mw_acc_get_acceleration_data_signal(self.device.board)
+        gyro = libmetawear.mbl_mw_gyro_bmi160_get_rotation_data_signal(self.device.board)
+
+        signals = (c_void_p * 1)()
+        signals[0] = gyro
+
+        # //libmetawear.mbl_mw_dataprocessor_accounter_create(signals, None, fn_wrapper)
+        
+        # chain two processors together (fuser and accounter) to get timestamped acc+gyro data
+        # create a fuser "data processor" which packages the acc and gyro signals into same packets before sending
+        fuser = create_voidp(lambda fn: libmetawear.mbl_mw_dataprocessor_fuser_create(acc, signals, 1, None, fn), resource = "fuser", event = e)
+        # accounter processor adds correct epoch data to BLE packets, necessary for timestamping stream-mode data
+        accounter = create_voidp(lambda fn: libmetawear.mbl_mw_dataprocessor_accounter_create(fuser, None, fn), resource = "accounter", event = e)
+
+        # //libmetawear.mbl_mw_datasignal_subscribe(self.processor, None, self.callback) 
+        libmetawear.mbl_mw_datasignal_subscribe(accounter, None, self.callback)
+
+    # begin sampling from gyro and acc signals
+    def start(self):
+        libmetawear.mbl_mw_gyro_bmi160_enable_rotation_sampling(self.device.board)
+        libmetawear.mbl_mw_acc_enable_acceleration_sampling(self.device.board)
+
+        libmetawear.mbl_mw_gyro_bmi160_start(self.device.board)
+        libmetawear.mbl_mw_acc_start(self.device.board)
+    
+    def reconfigure(self):
+        self.sensor_data = []
+        self.samples = 0
+    
+    def pause(self):
+        libmetawear.mbl_mw_acc_stop(self.device.board)
+        libmetawear.mbl_mw_gyro_bmi160_stop(self.device.board)
+        
+        # libmetawear.mbl_mw_acc_disable_acceleration_sampling(self.device.board)
+
+# iterates through MAC addresses provided as arguments in command line and attempts to connect to them
+for i in range(len(argv) - 1):
+    d = MetaWear(argv[i + 1])
+    d.connect()
+    print("Connected to " + d.address)
+    states.append(State(d))
+
+for s in states:
+    print("Configuring %s" % (s.device.address))
+    s.setup()
+    
+def streamData():
+    input("Press enter to start streaming...")
+    
+    # start streaming for all connected devices in states, reconfigure to clear previous data
+    for s in states:
+        s.reconfigure()
+        s.start()
+        
+    
+    #~ input("Press enter to stop streaming...")
+    #~ for s in states:
+        #~ s.pause()
+    
+    #~ print("\nPausing streaming...")
+    #~ sleep(2)
+    
+    while True: 
+        userInput = input("Enter mode (q - end): ")
+    
+        modes = ['LGW','SA','SD','RA','RD']
+    
+        if userInput in modes:
+                s.mode = modes.index(userInput) + 1
+               
+        else:
+                for s in states:
+                        s.pause()
+                
+                print("\n Pausing streaminng...")
+                sleep(2)
+                break
+    
+
+    trial_name = input("\nPlease enter trial name: ")
+
+    for idx, s in enumerate(states):
+        s.sensor_data = np.asarray(s.sensor_data, dtype=np.float64)
+        
+        # time based off epoch, stored in ms.
+        # remove offset and convert elapsed time to seconds
+        timeStart = s.sensor_data[0,0]
+        s.sensor_data[:,1] = [(x - timeStart) / 1000 for x in s.sensor_data[:,0]]
+
+        filename = '../TestData/' + trial_name + '_' + str(idx) + '.csv'
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        # write each sensor's data to a separate .csv file
+        with open(filename, 'w') as f:
+            f.write('epoch, elapseds, xacc, yacc, zacc, xgyro, ygyro, zgyro, mode\n')
+            for row in s.sensor_data:
+                f.write('%d,%.3f,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f,%d\n' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
+
+        print("Data saved to \'" + filename + "\'")
+
+    print("\nTotal Samples Received")
+    for s in states:
+        print("%s -> %d" % (s.device.address, s.samples))
+
+def reset():
+    print("Resetting devices")
+    events = []
+    for s in states:
+        e = Event()
+        events.append(e)
+
+        s.device.on_disconnect = lambda s: e.set()
+        libmetawear.mbl_mw_debug_reset(s.device.board)
+
+    for e in events:
+        e.wait()
+
+    #~ for s in states: # code from full_reset.py
+        #~ libmetawear.mbl_mw_logging_stop(s.device.board)
+        #~ libmetawear.mbl_mw_logging_clear_entries(s.device.board)
+        #~ libmetawear.mbl_mw_macro_erase_all(s.device.board)
+        #~ libmetawear.mbl_mw_debug_reset_after_gc(s.device.board)
+        #~ print("Erase logger and clear all entries")
+        #~ sleep(1.0)
+
+        #~ libmetawear.mbl_mw_debug_disconnect(s.device.board)
+        #~ sleep(1.0)
+
+        #~ s.device.disconnect()
+        #~ print("Disconnect")
+        #~ sleep(1.0)
+
+
+while True:
+    streamData()
+    if(input("\nWould you like to quit? y or n: \n")=='y'):
+        reset()
+        break
+
+
